d016bb0cfdd7cf986878612114c7ac9dec28e6fe
Yaasi98 <123585619+Yaasi98@users.noreply.github.com>
2023-07-18 11:37:35 +0200

    Pinis Hit Script, Grab Interactable Bowling

---
+++ VRBase/Assets/Scripts/TutorialController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialController : MiniGameController
+{
+    protected override void OnStart()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void OnStop()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
