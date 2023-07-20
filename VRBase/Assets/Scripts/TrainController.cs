a2a30d29bb6ab2895bb5463f265be99afa7a637d
Ferdinand Göpfert <ferdinand.goepfert@hs-furtwangen.de>
2023-07-20 21:16:56 +0200

    integrate System

---
+++ VRBase/Assets/Scripts/TrainController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainController : MiniGameController
+{
+    protected override void OnStart()
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    protected override void OnStop()
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    protected override void OnUpdate()
+    {
+        //throw new System.NotImplementedException();
+    }
+    protected override void OnAfterCloseScene()
+    {
+        //base.OnAfterCloseScene();
+    }
+    protected override void OnSceneCloser()
+    {
+        //base.OnSceneCloser();
+    }
+    protected override void OnSceneStarter()
+    {
+        //base.OnSceneStarter();
+    }
+}
